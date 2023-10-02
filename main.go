50ad248115bb880c9519c7548e3846add4da695b
PettersonSantos <petterson.santos@outlook.com.br>
2023-10-01 21:48:20 -0300

    more algorithms code

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
+	sr := []int{1,3,5,6}
 
-	strs := "[({})]"
-	fmt.Println(isValid(strs))
+	fmt.Print(searchInsert(sr, 2))
 
 }
 func isMonotonic(nums []int) bool {
@@ -109,3 +109,49 @@ func isValid(str string) bool {
 	}
 	return false
 }
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
+	}
+
+	return nil
+}
+
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func searchInsert(nums []int, target int) int {
+
+
+	for i :=0; i < len(nums); i++ {
+		if target <= nums[i] {
+			return i
+		}
+
+		if target == nums[i] {
+			return i
+		}
+	}
+
+
+	return len(nums)
+}
+
+
+
